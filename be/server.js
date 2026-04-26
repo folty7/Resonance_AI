3680715cdc2f4352eaa4317585bfe962505a52d7
folty7 <foland717@gmail.com>
2026-04-26 14:16:06 +0200

    feat: add x-gemini-api-key to CORS allowed headers in server configuration

--- be/server.js
+++ be/server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
     // Required for cookies
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials, x-gemini-api-key');
 
     // Chrome Private Network Access Preflight
     if (req.headers['access-control-request-private-network']) {
